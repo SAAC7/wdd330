fa43dbeae62c243603035fec15016d97bb53e641
SAAC7_Hybrid <saulabraham881@gmail.com>
2026-03-07 12:02:45 -0600

    i fix the cart

--- src/js/product.js
+++ src/js/product.js
@@ -1,10 +1,15 @@
-import { setLocalStorage } from './utils.mjs';
+import { getLocalStorage, setLocalStorage } from './utils.mjs';
 import ProductData from './ProductData.mjs';
 
 const dataSource = new ProductData('tents');
 
 function addProductToCart(product) {
-  setLocalStorage('so-cart', product);
+  let cartitems = getLocalStorage('so-cart')
+  if (!Array.isArray(cartitems)) {
+    cartitems = [];
+  }
+  cartitems.push(product)
+  setLocalStorage('so-cart', cartitems);
 }
 // add to cart button event handler
 async function addToCartHandler(e) {
