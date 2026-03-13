a71da4bd4d76a4cc63ea29a8548c2d65cbacc646
dani71296 <israel.tudela712.96@gmail.com>
2026-03-12 22:23:52 -0600

    feat: complete dynamic product list and vite configuration

--- src/js/utils.mjs
+++ src/js/utils.mjs
@@ -27,3 +27,11 @@ export function getParam(param) {
   const urlParams = new URLSearchParams(queryString);
   return urlParams.get(param);
 }
+// Esta función servirá para renderizar cualquier lista en el futuro
+export function renderListWithTemplate(templateFn, parentElement, list, position = "afterbegin", clear = false) {
+  if (clear) {
+    parentElement.innerHTML = "";
+  }
+  const htmlStrings = list.map(templateFn);
+  parentElement.insertAdjacentHTML(position, htmlStrings.join(""));
+}
