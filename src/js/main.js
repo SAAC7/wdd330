389456431e979b23cff653133c8d1952681800c7
SAAC7_Hybrid <saulabraham881@gmail.com>
2026-03-21 12:08:55 -0600

    intento de tarea

--- src/js/main.js
+++ src/js/main.js
@@ -1,24 +1,7 @@
-import ProductData from './ProductData.mjs';
-import ProductList from './ProductList.mjs';
-import { updateCartCount,loadHeaderFooter } from './utils.mjs';
+import { loadHeaderFooter } from './utils.mjs';
 import Alert from './alert.js';
-
-
-// 1. Creamos el cargador de datos para 'tents' (carpas)
-const dataSource = new ProductData('tents');
-
-// 2. Seleccionamos el elemento <ul> del HTML donde irán los productos
-const element = document.querySelector('.product-list');
-
-// 3. Creamos la lista y la inicializamos
-const listing = new ProductList('tents', dataSource, element);
-
-
-
 
 const alerts = new Alert('../json/alerts.json');
 alerts.init();
 
 loadHeaderFooter();
-
-listing.init();
---
+++ src/js/product-listing.js
@@ -0,0 +1,14 @@
+import ProductData from './ProductData.mjs';
+import ProductList from './ProductList.mjs';
+
+
+// 1. Creamos el cargador de datos para 'tents' (carpas)
+const dataSource = new ProductData('tents');
+
+// 2. Seleccionamos el elemento <ul> del HTML donde irán los productos
+const element = document.querySelector('.product-list');
+
+// 3. Creamos la lista y la inicializamos
+const listing = new ProductList('tents', dataSource, element);
+
+listing.init();
